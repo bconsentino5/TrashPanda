0ced653fba5719a54288de99c2785d570948bbd3
bellac5 <isco9616@colorado.edu>
2025-12-05 14:42:12 -0700

    .

--- Xp.gd
+++ Xp.gd
@@ -3,6 +3,9 @@ class_name _XP
 
 signal update_xp
 
+func _ready() -> void:
+	$AnimationPlayer.play("bounce")
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		self.queue_free()
