63d7235564379e7b9d862a56a2483521b487f7cf
bellac5 <isco9616@colorado.edu>
2025-12-18 19:48:26 -0500

    .

--- Xp.gd
+++ Xp.gd
@@ -3,8 +3,15 @@ class_name _XP
 
 signal update_xp
 
+@onready var sprite = $Sprite2D
+
+var cheese = preload("res://Assets/cheese.png")
+var candy = preload("res://Assets/CANDY.png")
+
 func _ready() -> void:
 	$AnimationPlayer.play("bounce")
+	if get_tree().current_scene.name == "L2":
+		sprite.texture = cheese
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
